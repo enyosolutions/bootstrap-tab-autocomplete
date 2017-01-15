a39747254a330d704a5aa5dea71def93c15147f5
Enyosolutions <fb@enyosolutions.com>
2017-01-15 02:00:02 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+  "name": "bootstrap-tab-autocomplete",
+  "version": "0.0.1",
+  "description": "Bootstrap autocomplete in tabs",
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/enyosolutions/bootstrap-tab-autocomplete.git"
+  },
+  "author": "makmakulet",
+  "license": "ISC",
+  "bugs": {
+    "url": "https://github.com/enyosolutions/bootstrap-tab-autocomplete/issues"
+  },
+  "homepage": "https://github.com/enyosolutions/bootstrap-tab-autocomplete#readme"
+}
